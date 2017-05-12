ed5916739d8d218aaadbd99e200acad65a39f7cd
Bernice Tan <bernice.tann@gmail.com>
2017-05-12 02:17:37 +0000

    made changes to helpers

---
+++ helpers/helperfunctions.js
@@ -0,0 +1,9 @@
+module.exports = {
+  generatedRand: function() {
+    var text = "";
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 6; i++)
+      text = text + possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+}
